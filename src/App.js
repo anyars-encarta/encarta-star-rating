f3ec52783a490ff3e404dc509b45ccb5ca8070d7
anyars-encarta <anyarsencarta@gmail.com>
2024-02-11 12:38:44 +0000

    Correct eslint errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import StarRating from './components/star-rating/index';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <StarRating noOfStars={10}/>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <StarRating noOfStars={10} />
+  </div>
+);
 
 export default App;
